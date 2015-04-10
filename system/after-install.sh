61191b9fb73021b1f7707ef60a3dd39c5481c3c2
ajduke <ajduke@about.me>
2015-04-10 18:06:53 +0530

    added the after install steps

--- system/after-install.sh
+++ system/after-install.sh
@@ -1,3 +1,21 @@
+
+# open and edit ~/.zshrc file to replace all content with following-
+source ~/dotfiles/zshell/.zshrc
+
+# copy git specific files
+cp ~/dotfiles/git/.gitconfig ~/.gitconfig 
+cp ~/dotfiles/git/.gitignore ~/.gitignore
+
+# copy the terminator config file 
+cp ~/dotfiles/terminator/config ~/.config/terminator/config 
+
+#tmux configuration directory
+sudo cp ~/dotfiles/tmux/tmux.conf /etc/tmux.conf
+
+# install flashplayer 
+# Go to software center
+# search for pepper flash player and install it 
+
 # create file called git-diff.sh and paste following content 
 #!/bin/bash 
 meld "$2" "$5" > /dev/null 2>&1
@@ -6,18 +24,14 @@ meld "$2" "$5" > /dev/null 2>&1
 sudo mv git-diff.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/git-diff.sh
 
-
 # elastic home directory 
 # /usr/share/elasticsearch
 # elastic search configuration
 # /etc/elasticsearch/
 
-#tmux configuration directory
-#/etc/tmux
-
 # install head plugin 
 sudo /usr/share/elasticsearch/bin/plugin --install mobz/elasticsearch-head
 
-
 ## download and install ubuntu docker image
 sudo docker run -i -t ubuntu /bin/bash
+
